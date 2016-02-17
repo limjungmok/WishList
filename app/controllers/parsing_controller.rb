6bf84c60b02975f5497330c2ebacf5b8725dd39b
JM <limjungmok@gmail.com>
2016-02-17 16:08:08 +0900

    auction wemakeprice

--- app/controllers/parsing_controller.rb
+++ app/controllers/parsing_controller.rb
@@ -13,12 +13,15 @@ class ParsingController < ApplicationController
 
         # 액션과 연결시켜 주는 곳
         puts case sUrlOriginal
-
         when "http://store.musinsa.com"
         	musinsa(sUrl)
 
         when "http://www.zara.com"
 
+        when "http://itempage3.auction.co.kr"
+            auction(sUrl)
+        when "http://www.wemakeprice.com"
+            wemakeprice(sUrl)
         end
 
         if @@b_in == false
@@ -59,5 +62,47 @@ class ParsingController < ApplicationController
     	end
 
     	@@b_in = true
+    end
+
+    def wemakeprice(url)
+        doc = Nokogiri::HTML(open(url))
+   
+
+        title_s = doc.css('h4').children[0].text
+        title = title_s[4..title_s.length]
+        price = doc.css('li').css('.sale').children[0].children.text
+        price_s = price.split ","
+        price = price_s[0] + price_s[1]
+        img = doc.css('meta')[9].attributes['content'].value
+       
+        data = {:message => "success", :title => title, :price => price ,:img => img, :url => url}
+        
+        respond_to do |format|
+            format.html
+            format.json { render :json => data }
+        end
+
+        @@b_in = true 
+    end
+
+
+    def auction(url)
+        doc = Nokogiri::HTML(open(url).read.encode('utf-8', 'euc-kr'))
+
+        title = doc.css('.product div')[0].children[1].attributes['alt'].value
+        price_origin = doc.css('.product div')[1].children.children[2].children.text
+        price_temp = price_origin.split "원"
+        price_s = price_temp[0].split ","
+        price = price_s[0] + price_s[1]
+        img = doc.css('.product div')[0].children[1].attributes['src'].value
+
+        data = {:message => "success", :title => title, :price => price ,:img => img, :url => url}
+
+        respond_to do |format|
+            format.html
+            format.json { render :json => data }
+        end
+
+        @@b_in = true 
     end
 end
