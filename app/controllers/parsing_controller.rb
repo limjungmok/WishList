6dd04a790676bcb46b90f7c95ce5939d6dd5a398
진 여준 <ye0jun@jin-ui-MacBook-Pro.local>
2016-02-17 16:11:56 +0900

    breakcomma 11st coupang

--- app/controllers/parsing_controller.rb
+++ app/controllers/parsing_controller.rb
@@ -5,9 +5,10 @@ class ParsingController < ApplicationController
 	def findParsingAction()
 
 		sUrl = params[:url]
-        #sUrl = "store.musinsa.com/app/product/detail/282220/0"
+		
 
-        if sUrl.index("/",8) != nil
+		byebug
+		if sUrl.index("/",8) != nil
             sUrlOriginal = sUrl[0,sUrl.index("/",8)] # url 중 original 주소만 가져 옴
         end
 
@@ -19,6 +20,12 @@ class ParsingController < ApplicationController
 
         when "http://www.zara.com"
 
+        when "http://www.11st.co.kr" , "http://deal.11st.co.kr"
+        	for_11st = params[:url]+"&prdNo="+params[:prdNo]
+        	_11st(for_11st)
+
+        when "https://www.coupang.com"
+        	coupang(sUrl)
         end
 
         if @@b_in == false
@@ -36,8 +43,14 @@ class ParsingController < ApplicationController
 
     #사이트 기본 액션 템플렛
     # def site_name(url)
-    #     doc = Nokogiri::HTML(open(url))
-    #     doc.css("meta")[5]['content']
+    #  	doc = Nokogiri::HTML(open(url))
+    # 	doc.css("meta")[5]['content']
+    # 	data = {:message => "success", :title => title, :price => price ,:img => img, :url => url}
+    # 	respond_to do |format|
+    # 		format.html
+    # 		format.json { render :json => data }
+    # 	end
+    # 	@@b_in = true
     # end
 
     def musinsa(url)
@@ -59,5 +72,50 @@ class ParsingController < ApplicationController
     	end
 
     	@@b_in = true
+    end
+
+    def _11st(url)
+     	doc = Nokogiri::HTML(open(url))
+    	title = doc.css("title")[0].text
+    	price = doc.xpath("//meta[@property='price']/@content")[0].value
+    	img = doc.xpath("//meta[@property='og:image']/@content")[0].value
+
+    	data = {:message => "success", :title => title, :price => price ,:img => img, :url => url}
+    	respond_to do |format|
+    		format.html
+    		format.json { render :json => data }
+    	end
+    	@@b_in = true
+    end
+
+    def coupang(url)
+    	doc = Nokogiri::HTML(open(url))
+
+    	title = doc.xpath("//meta[@property='og:title']/@content")[0].value
+    	img = doc.xpath("//meta[@property='og:image']/@content")[0].value
+    	price_original = doc.xpath("//strong[@class='price']").text.split ","
+    	price = price_original[0] + price_original[1]
+
+    	data = {:message => "success", :title => title, :price => price ,:img => img, :url => url}
+    	respond_to do |format|
+    		format.html
+    		format.json { render :json => data }
+    	end
+    	@@b_in = true
+    end
+
+    def breakcomma(price)
+
+    	if price.index(",") == nil
+
+    	else
+    		separate_price = price.split ","
+    		price = String.new
+    		separate_price.each do |f|
+    			price = price + f
+    		end
+    	end
+
+    	return price;
     end
 end
