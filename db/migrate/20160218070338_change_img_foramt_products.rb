8602bf312e9d1fd45d3a2d9d19ccc842e04529be
진 여준 <ye0jun@jin-ui-MacBook-Pro.local>
2016-02-18 18:09:00 +0900

    save the server

--- db/migrate/20160218070338_change_img_foramt_products.rb
+++
@@ -1,6 +0,0 @@
-class ChangeImgForamtProducts < ActiveRecord::Migration
-  def change
-    remove_column :products, :img
-    add_column :products, :img, :text
-  end
-end
--- db/migrate/20160218071403_change_img_real_products.rb
+++
@@ -1,6 +0,0 @@
-class ChangeImgRealProducts < ActiveRecord::Migration
-  def change
-	change_column :products, :img, :text, :limit => nil
-
-  end
-end
